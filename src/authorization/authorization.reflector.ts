05b4c61d8e3089f0d106aee901e4794c978c608a
Roy OSSAI <roy.ossai@bpifrance.fr>
2025-02-01 11:08:11 +0100

    update to custom reflector

--- src/authorization/authorization.reflector.ts
+++ src/authorization/authorization.reflector.ts
@@ -1,13 +1,17 @@
 import 'reflect-metadata';
 
 export class AuthorizationReflector {
-    get<T> (metadataKey: string | symbol, target: any): T | undefined {
+    get<T> (metadataKey: string | symbol, target: any, propertyKey?: string | symbol): T | undefined {
+        if (propertyKey) {
+            return Reflect.getMetadata(metadataKey, target, propertyKey);
+        }
+
         return Reflect.getMetadata(metadataKey, target);
     }
 
-    getAllAndOverride<T> (metadataKey: string | symbol, targets: any[]): T | undefined {
-        for (const target of targets) {
-            const metadata = Reflect.getMetadata(metadataKey, target);
+    getAllAndOverride<T> (metadataKey: string | symbol, targets: [any, any?]): T | undefined {
+        for (const [target, propertyKey] of targets) {
+            const metadata = Reflect.getMetadata(metadataKey, target, propertyKey);
 
             if (metadata !== undefined) {
                 return metadata;
@@ -17,17 +21,17 @@ export class AuthorizationReflector {
         return undefined;
     }
 
-    getAllAndMerge<T> (metadataKey: string | symbol, targets: any[]): T extends any[] ? T : T[] {
-        const result: any[] = [];
+    getAllAndMerge<T> (metadataKey: string | symbol, targets: [any, any?][]): T extends any[] ? T : T[] {
+        const result: any = [];
 
-        for (const target of targets) {
-            const metadata = Reflect.getMetadata(metadataKey, target);
+        for (const [target, propertyKey] of targets) {
+            const metadata = Reflect.getMetadata(metadataKey, target, propertyKey);
 
             if (metadata) {
                 result.push(...(Array.isArray(metadata) ? metadata : [metadata]));
             }
         }
 
-        return result as T extends any[] ? T : T[];
+        return result;
     }
 }
